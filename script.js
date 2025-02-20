a5e032e9874d918e16da599a9732141fb0f25bab
deepak-2610 <141878261+deepak-2610@users.noreply.github.com>
2025-02-20 17:14:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var typeData = new Typed(".text", {
+    strings: [
+      "Frontend Developer",
+      "Web Developer",
+      "Competive programmer",
+      "Graphics Designer",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+  });
